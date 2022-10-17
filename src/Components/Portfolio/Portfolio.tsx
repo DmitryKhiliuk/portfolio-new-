6e03af1da55df281341ce901ccfd97b739e7cf6f
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-10-17 17:19:28 +0300

    fix ToDo

--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -25,7 +25,7 @@ export const Portfolio = () => {
             <div className={s.portfolio__container}>
                 <Title img={portfolio}/>
                 <div className={s.projects}>
-                    <Project background={bgTodoList}  addressProject={'https://DmitryKhiliuk.github.io/ToDo.git'} addressCode={'https://github.com/DmitryKhiliuk/ToDo'}  title={'Todolist'} text={'Technologies: TypeScript, Redux Toolkit, RestAPI, Redux-thunk, Material UI, Unit Tests, Storybook'}/>
+                    <Project background={bgTodoList}  addressProject={'https://DmitryKhiliuk.github.io/ToDo/'} addressCode={'https://github.com/DmitryKhiliuk/ToDo'}  title={'Todolist'} text={'Technologies: TypeScript, Redux Toolkit, RestAPI, Redux-thunk, Material UI, Unit Tests, Storybook'}/>
                     <Project background={bgBookshelf} addressProject={'https://DmitryKhiliuk.github.io/Bookshelf'} addressCode={'https://github.com/DmitryKhiliuk/Bookshelf.git'} title={'Bookshelf'} text={'Technologies: TypeScript, Redux, RestAPI, Redux-thunk'}/>
                     <Project background={bgSocialNetwork} addressProject={'https://dmitrykhiliuk.github.io/social-network-port'} addressCode={'https://github.com/DmitryKhiliuk/social-network-port.git'} title={'Social Network'} text={'Technologies: TypeScript, React, Redux, Redux-thunk, Redux-form, Axios, Reselect, SCSS. Use common test account credentials: Email: free@samuraijs.com Password: free'}/>
                     <Project background={bgTrainingCards} addressProject={'https://DmitryKhiliuk.github.io/cards'} addressCode={'https://github.com/DmitryKhiliuk/cards.git'} title={'Training Cards'} text={'Technologies: TypeScript, Redux, RestAPI, Redux-thunk, Material UI'}/>
